091ea2dea65d7881bc931254c41203eb4e85cb86
AndrewsKanyi <mrdrew.af@gmail.com>
2020-12-07 14:25:07 +0100

    Add splash screen to project

--- app/src/main/java/com/example/ghanatunes/MainActivity.kt
+++ app/src/main/java/com/example/ghanatunes/MainActivity.kt
@@ -1,39 +1,28 @@
 package com.example.ghanatunes
 
 import android.os.Bundle
+import android.os.Handler
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.constraintlayout.widget.ConstraintLayout
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val view = findViewById<ConstraintLayout>(R.id.myCoordinatorLayout)
+        val r: Runnable = object : Runnable {
+            override fun run() {
+                Snackbar.make(view, "Printing  text after delay", Snackbar.LENGTH_LONG)
+                    .show()
+            }
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        val mHandler = Handler();
+        mHandler.postDelayed(r, 5000)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
